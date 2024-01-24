b7899fa3689fe842c0c2a564977e1f34e51364db
iflamei <iflamei@mail.ru>
2024-01-24 13:54:58 +0700

    getMenus,getFood,createFood methods were created

--- models/userModel.go
+++ models/userModel.go
@@ -18,5 +18,5 @@ type User struct {
 	Refresh_Token *string            `json:"refresh_Token"`
 	Created_at    time.Time          `json:"created_at"`
 	Updated_at    time.Time          `json:"updated_at"`
-	User_id       string             `json:"user_id" `
+	User_id       string             `json:"user_id"`
 }
